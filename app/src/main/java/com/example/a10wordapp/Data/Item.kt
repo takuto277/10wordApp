fae3f063654d1f421d3082aed71b069b9288a54c
Takuto <Takuto.72.soccer@gmail.com>
2023-01-02 18:01:06 +0900

    fix_secondActivity.kt

---
+++ app/src/main/java/com/example/a10wordapp/Data/Item.kt
@@ -0,0 +1,22 @@
+package com.example.a10wordapp.Data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.text.NumberFormat
+
+@Entity(tableName = "item_table")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "english")
+    val english: String,
+    @ColumnInfo(name = "japanese")
+    val japanese: String
+    )
+    /**
+     * Returns the passed in price in currency format.
+     */
+//    fun Item.getFormattedPrice(): String =
+//        NumberFormat.getCurrencyInstance().format(itemPrice)
+//}
--- app/src/main/java/com/example/a10wordapp/ThirdActivity.kt
+++ app/src/main/java/com/example/a10wordapp/ThirdActivity.kt
@@ -3,15 +3,36 @@ package com.example.a10wordapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.a10wordapp.databinding.ActivitySecondBinding
+import com.example.a10wordapp.databinding.ActivityThirdBinding
+import com.example.a10wordapp.ViewModel
+import androidx.activity.viewModels
 
 class ThirdActivity : AppCompatActivity() {
+
+    private val viewModel: ViewModel by viewModels()
+    private lateinit var binding: ActivityThirdBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_third)
         val btnBack: Button = findViewById(R.id.btnBack)
 
-        btnBack.setOnClickListener {
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+
+        val view = binding.root
+
+        setContentView(view)
+
+        binding.saveAction.setOnClickListener {
+            addNewItem(1, "aa", "bb")
+        }
+
+        binding.buckAction.setOnClickListener {
             finish()
         }
+    }
+
+    private fun addNewItem(id: Int, english: String, japanese: String){
+        viewModel.addNewItem(id, english, japanese)
+
     }
 }
