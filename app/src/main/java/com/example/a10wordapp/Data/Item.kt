f26752b237287a1e80fd3a634688e11949ab6c6f
Takuto <Takuto.72.soccer@gmail.com>
2023-01-10 22:18:27 +0900

    Databaseを追加

--- app/src/main/java/com/example/a10wordapp/Data/Item.kt
+++ app/src/main/java/com/example/a10wordapp/Data/Item.kt
@@ -1,4 +1,19 @@
-//package com.example.a10wordapp.Data
+package com.example.a10wordapp.Data
+
+import androidx.room.PrimaryKey
+import androidx.room.Entity
+import androidx.room.ColumnInfo
+
+@Entity
+data class Item(
+    @PrimaryKey
+    val id: Int,
+    @ColumnInfo(name = "english")
+    val english: String,
+    @ColumnInfo(name = "japanese")
+    val japanese: String
+
+)
 //
 //import androidx.room.ColumnInfo
 //import androidx.room.Entity
--- app/src/main/java/com/example/a10wordapp/Data/ItemDao.kt
+++ app/src/main/java/com/example/a10wordapp/Data/ItemDao.kt
@@ -1,27 +1,27 @@
-//package com.example.a10wordapp.Data
-//
-//import androidx.lifecycle.LiveData
-//import androidx.room.Dao
-//import androidx.room.Delete
-//import androidx.room.Insert
-//import androidx.room.OnConflictStrategy
-//import androidx.room.Query
-//import androidx.room.Update
-//import kotlinx.coroutines.flow.Flow
-//
-///**
-// * Database access object to access the Inventory database
-// */
-//@Dao
-//interface ItemDao {
-//
-//    @Query("SELECT * FROM item_table")
-//    fun loadLiveData(): LiveData<List<Item>>
-//
-//    @Insert
-//    suspend fun insert(item: Item)
-//
-//    @Delete
-//    suspend fun delete(item: Item)
-//
-//}
+package com.example.a10wordapp.Data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Database access object to access the Inventory database
+ */
+@Dao
+interface ItemDao {
+
+    @Query("SELECT * FROM item")
+    fun getAll(): List<Item>
+
+    @Insert
+    fun insert(item: Item)
+
+    @Delete
+    fun delete(item: Item)
+
+}
