3a741bef5989718b8066b21f05f5e8ef9182a1e6
Takuto <Takuto.72.soccer@gmail.com>
2023-01-14 20:10:38 +0900

    Add_observeのテンプレート

--- app/src/main/java/com/example/a10wordapp/ui/list/ListActivity.kt
+++ app/src/main/java/com/example/a10wordapp/ui/list/ListActivity.kt
@@ -7,6 +7,8 @@ import android.widget.ListView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
 
 class
 ListActivity : AppCompatActivity(){
@@ -26,6 +28,14 @@ ListActivity : AppCompatActivity(){
         listView.setOnItemClickListener{ adapter, view, position, id ->
             val message = "「${(array[position])}」をクリックしました。"
             Toast.makeText(this@ListActivity, message, Toast.LENGTH_SHORT).show()
+            viewModel.hoge()
         }
+
+        viewModel.liveData.observe(this, Observer {
+            Snackbar.make(listView, "押されたよ", Snackbar.LENGTH_LONG).show()
+        })
+        viewModel.listLiveData.observe(this, Observer {
+            Snackbar.make(listView, "ListLiveDataです", Snackbar.LENGTH_LONG).show()
+        })
     }
 }
--- app/src/main/java/com/example/a10wordapp/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/a10wordapp/ui/list/ListViewModel.kt
@@ -2,6 +2,8 @@ package com.example.a10wordapp.ui.list
 
 import android.content.Context
 import android.widget.ListView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.a10wordapp.Data.ItemEntiry
 import com.example.a10wordapp.Repository.AddArrayRepository
@@ -9,14 +11,23 @@ import com.example.a10wordapp.Repository.GetListViewRepository
 import com.example.a10wordapp.Repository.RoomRepository
 
 open class ListViewModel: ViewModel() {
+    private val mutableLiveData = MutableLiveData<String>()
+    private val mutableLiveDataList = MutableLiveData<List<ItemEntiry>>()
+    val liveData: LiveData<String> = mutableLiveData
+    val listLiveData: LiveData<List<ItemEntiry>> = mutableLiveDataList
+    fun hoge() {
+        mutableLiveData.value = "なんか起きた"
+    }
     fun getList(context: Context): List<ItemEntiry> {
         var list = RoomRepository().getList(context)
-        return list
+        mutableLiveDataList.value = RoomRepository().getList(context)
+            return list
         //TODO:返り値を使わずにobserveを実装する予定
     }
 
     fun getListView(context: Context): ListView{
         var listView = GetListViewRepository().getListView(context)
+
         return  listView
     }
 
