e3ad5f4fc986a298754189b012ae16fdba2f53ff
Takuto <Takuto.72.soccer@gmail.com>
2023-01-22 19:48:20 +0900

    Add_TextLayout(断念)

--- app/src/main/java/com/example/a10wordapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/a10wordapp/ui/home/HomeActivity.kt
@@ -9,6 +9,7 @@ import com.example.a10wordapp.ui.questionlist.QuestionListActivity
 import com.example.a10wordapp.ui.quiz.QuizActivity
 import com.example.a10wordapp.ui.add.AddActivity
 import com.example.a10wordapp.ui.delete.DeleteActivity
+import com.example.a10wordapp.ui.selectQuiz.SelectQuizActivity
 
 
 class HomeActivity : AppCompatActivity() {
@@ -21,7 +22,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.learnButton.setOnClickListener {
-            val intent = Intent(this, QuizActivity::class.java)
+            val intent = Intent(this, SelectQuizActivity::class.java)
             startActivity(intent)
         }
         binding.addButton.setOnClickListener {
