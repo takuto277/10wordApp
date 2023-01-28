402a7d4212a61ac81afc90745a29b2b955cc2a3f
Takuto <Takuto.72.soccer@gmail.com>
2023-01-28 23:33:05 +0900

    Fix_ViewModelFactory

---
+++ app/src/main/java/com/example/a10wordapp/api/InitialDataAPI.kt
@@ -0,0 +1,20 @@
+package com.example.a10wordapp.api
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class InitialDataAPI {
+    val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://gist.githubusercontent.com/")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+
+    val service: InitialDataService = retrofit.create(InitialDataService::class.java)
+
+}
