f36eb08867759bd848d6a6b7bf7ceddc1f273a97
Takuto <Takuto.72.soccer@gmail.com>
2023-01-30 00:22:34 +0900

    Fix_conflict

--- app/src/main/java/com/example/a10wordapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/a10wordapp/ui/home/HomeActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.room.Query
+import com.example.a10wordapp.ViewModelFactory
 import com.example.a10wordapp.api.InitialDataAPI
 import com.example.a10wordapp.api.InitialDataResponse
 import com.example.a10wordapp.api.InitialDataService
@@ -58,5 +59,13 @@ class HomeActivity : AppCompatActivity() {
         binding.apiFetchButton.setOnClickListener {
             viewModel.fetchAPI()
         }
+
+        viewModel.english.observe(this, Observer { english ->
+            Toast.makeText(
+                this@HomeActivity,
+                "「${english}」を表示します。",
+                Toast.LENGTH_SHORT
+            ).show()
+        })
     }
 }
