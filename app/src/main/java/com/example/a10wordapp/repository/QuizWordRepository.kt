58eca88c8b934291a884c453886938f47c65dfd7
Takuto <Takuto.72.soccer@gmail.com>
2023-02-02 22:29:23 +0900

    Add_QuizWordRepositoryのInterface

--- app/src/main/java/com/example/a10wordapp/repository/QuizWordRepository.kt
+++ app/src/main/java/com/example/a10wordapp/repository/QuizWordRepository.kt
@@ -7,22 +7,25 @@ import com.example.a10wordapp.data.db.entity.InitialDataEntity
 import com.example.a10wordapp.data.db.entity.ItemEntity
 import kotlinx.coroutines.runBlocking
 
-class QuizWordRepository(private val context: Context) {
-    fun addNewItem(id: Int, english: String, japanese: String) {
+interface QuizWordRepository {
+    fun addNewItem(id: Int, english: String, japanese: String)
+    suspend fun saveInitialData(data: Array<Data>)
+    fun getList(): List<ItemEntity>
+    fun getInitialDataList(): List<InitialDataEntity>
+
+}
+
+class QuizWordRepositoryImpl(private val context: Context): QuizWordRepository {
+    override fun addNewItem(id: Int, english: String, japanese: String) {
         val getDatabase = AppRoomDatabase.getDatabase(context)
         val itemEntiry = ItemEntity(id, english, japanese)
         val itemDao = getDatabase.itemDao()
         runBlocking {
             itemDao.insert(itemEntiry)
         }
-
-        var list: List<ItemEntity>
-        runBlocking {
-            list = itemDao.getAll()
-        }
     }
 
-    suspend fun saveInitialData(data: Array<Data>) {
+    override suspend fun saveInitialData(data: Array<Data>) {
         val getDatabase = AppRoomDatabase.getDatabase(context)
         val initialDataDao = getDatabase.initialDataDao()
         if (initialDataDao.getAll().isNotEmpty()) {
@@ -36,7 +39,7 @@ class QuizWordRepository(private val context: Context) {
         }
     }
 
-    fun getList(): List<ItemEntity> {
+    override fun getList(): List<ItemEntity> {
         val getDatabase = AppRoomDatabase.getDatabase(context)
         val itemDao = getDatabase.itemDao()
 
@@ -47,7 +50,7 @@ class QuizWordRepository(private val context: Context) {
         return list
     }
 
-    fun getInitialDataList(): List<InitialDataEntity> {
+    override fun getInitialDataList(): List<InitialDataEntity> {
         val getDatabase = AppRoomDatabase.getDatabase(context)
         val initialDataDao = getDatabase.initialDataDao()
 
