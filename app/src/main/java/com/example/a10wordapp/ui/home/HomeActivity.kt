0f13797f5db7ab2df604aaf6d1d3e6add2ad84e0
Takuto <Takuto.72.soccer@gmail.com>
2023-02-04 02:29:10 +0900

    HomeActivityをコメントアウト

--- app/src/main/java/com/example/a10wordapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/a10wordapp/ui/home/HomeActivity.kt
@@ -1,56 +1,58 @@
 package com.example.a10wordapp.ui.home
 
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import com.example.a10wordapp.databinding.ActivityHomeBinding
-import com.example.a10wordapp.ui.ViewModelFactory
-import com.example.a10wordapp.ui.quizadd.QuizAddActivity
-import com.example.a10wordapp.ui.quizdelete.QuizDeleteActivity
-import com.example.a10wordapp.ui.quizlist.QuizListActivity
-import com.example.a10wordapp.ui.quizselect.QuizSelectActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.a10wordapp.R
 
 
-class HomeActivity : AppCompatActivity() {
-    private val viewModel: HomeViewModel by viewModels { ViewModelFactory(applicationContext) }
-    private lateinit var binding: ActivityHomeBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.learnButton.setOnClickListener {
-            val intent = Intent(this, QuizSelectActivity::class.java)
-            startActivity(intent)
-        }
-        binding.addButton.setOnClickListener {
-            val intent = Intent(this, QuizAddActivity::class.java)
-            startActivity(intent)
-        }
-        binding.deleteButton.setOnClickListener {
-            val intent = Intent(this, QuizDeleteActivity::class.java)
-            startActivity(intent)
-        }
-        binding.allButton.setOnClickListener {
-            val intent = Intent(this, QuizListActivity::class.java)
-            startActivity(intent)
-        }
-        binding.apiFetchButton.setOnClickListener {
-            viewModel.fetchAPI()
-        }
-
-        viewModel.english.observe(this, Observer { english ->
-            Toast.makeText(
-                this@HomeActivity,
-                "「${english}」を表示します。",
-                Toast.LENGTH_SHORT
-            ).show()
-        })
+class HomeActivity : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_home, container, false)
     }
+//    private val viewModel: HomeViewModel by viewModels { ViewModelFactory(applicationContext) }
+//    private lateinit var binding: ActivityHomeBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        binding = ActivityHomeBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.learnButton.setOnClickListener {
+//            val intent = Intent(this, QuizSelectActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.addButton.setOnClickListener {
+//            val intent = Intent(this, QuizAddActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.deleteButton.setOnClickListener {
+//            val intent = Intent(this, QuizDeleteActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.allButton.setOnClickListener {
+//            val intent = Intent(this, QuizListActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.apiFetchButton.setOnClickListener {
+//            viewModel.fetchAPI()
+//        }
+//
+//        viewModel.english.observe(this, Observer { english ->
+//            Toast.makeText(
+//                this@HomeActivity,
+//                "「${english}」を表示します。",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        })
+//    }
 }
