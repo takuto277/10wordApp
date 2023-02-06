032d646f6758623785408d3bce5075e809cfb2d3
Takuto <Takuto.72.soccer@gmail.com>
2023-02-07 00:19:26 +0900

    Entityの追加

---
+++ app/src/main/java/com/example/a10wordapp/domain/entity/GetListEntity.kt
@@ -0,0 +1,7 @@
+package com.example.a10wordapp.domain.entity
+
+data class GetListEntity(
+    val id: Int,
+    val english: String,
+    val japanese: String
+)
---
+++ app/src/main/java/com/example/a10wordapp/domain/entity/QuizShowEntity.kt
@@ -0,0 +1,7 @@
+package com.example.a10wordapp.domain.entity
+
+data class QuizShowEntity(
+    val id: Int,
+    val english: String,
+    val japanese: String
+)
